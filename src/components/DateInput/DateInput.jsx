115433090d40c05bce15880805d212b8f84d6a51
Barrett Anderson <barrett1614@gmail.com>
2025-02-04 11:26:02 -0600

    Adjusting DateInput

--- src/components/DateInput/DateInput.jsx
+++ src/components/DateInput/DateInput.jsx
@@ -5,9 +5,30 @@ function DateInput() {
   return (
     <div className="date-input">
       <p>Placeholder</p>
-      <input type="text" placeholder="Month" />
-      <input type="text" placeholder="Day" />
-      <input type="text" placeholder="Year" />
+      <select>
+        <option value="">Month</option>
+        {Array.from({ length: 12 }, (_, i) => (
+          <option key={i + 1} value={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+      <select>
+        <option value="">Day</option>
+        {Array.from({ length: 31 }, (_, i) => (
+          <option key={i + 1} value={i + 1}>
+            {i + 1}
+          </option>
+        ))}
+      </select>
+      <select>
+        <option value="">Year</option>
+        {Array.from({ length: 2025 - 1900 + 1 }, (_, i) => (
+          <option key={1900 + i} value={1900 + i}>
+            {1900 + i}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
