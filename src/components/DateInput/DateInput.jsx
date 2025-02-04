f2ff423d61326bfc18f11914e345c97aa5974a29
Barrett Anderson <barrett1614@gmail.com>
2025-02-04 08:43:14 -0600

    Adjusting DateInput

--- src/components/DateInput/DateInput.jsx
+++ src/components/DateInput/DateInput.jsx
@@ -4,6 +4,7 @@ import "./DateInput.css";
 function DateInput() {
   return (
     <div className="date-input">
+      <p>Placeholder</p>
       <input type="text" placeholder="Month" />
       <input type="text" placeholder="Day" />
       <input type="text" placeholder="Year" />
